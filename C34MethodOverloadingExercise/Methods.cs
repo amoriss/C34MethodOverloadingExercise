a6c5d6207b9dc88b02844e5a71ac91375dfd0559
Amoriss <amoriss@protonmail.com>
2023-02-09 11:37:29 -0600

    Add project files.

---
+++ C34MethodOverloadingExercise/Methods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C34MethodOverloadingExercise;
+internal class Methods
+{
+
+    public static int Add(int num1, int num2)
+    {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static decimal Add(decimal num1, decimal num2)
+    {
+        decimal sum = num1 + num2;
+        return sum;
+    }
+
+    public static string Add(int num1, int num2, bool isMoney)
+    {
+        int sum = num1 + num2;
+        if (isMoney == true)
+        {
+            if (sum > 1 || sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            return $"{sum} dollar";
+        }
+        else
+        {
+            return $"{sum}";
+        }
+    }
+}
---
+++ C34MethodOverloadingExercise/Program.cs
@@ -0,0 +1,9 @@
+﻿using C34MethodOverloadingExercise;
+
+
+//Method Overloading -- using the same method name BUT must change the signature
+
+var answer = Methods.Add(53, 5643);
+Console.WriteLine(answer);
+
+Console.WriteLine(Methods.Add(0, 1000000000, true));
